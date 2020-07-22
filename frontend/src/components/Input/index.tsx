f34436ae8a108dff17d88f6d5e5328b654ad2cbe
Fernando Frascino <fernandofrascino@yahoo.com.br>
2020-07-22 12:27:50 -0300

    add: Tooltip component and sign up page finished

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,8 +1,9 @@
 import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
 
-import { Container } from './styles';
+import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -28,19 +29,19 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!! inputRef.current?.value);
+    setIsFilled(!!inputRef.current?.value);
   }, []);
 
   useEffect(() => {
     registerField({
       name: fieldName,
-      ref:  inputRef.current,
+      ref: inputRef.current,
       path: 'value',
     })
   }, [registerField, fieldName]);
 
   return (
-    <Container isFilled={isFilled} isFocused={isFocused}>
+    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
@@ -49,6 +50,11 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         ref={inputRef}
         {...rest}
       />
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
     </Container>
   )
 };
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,9 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import * as Yup from 'yup';
 
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+
+import getValidationsErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -12,8 +15,11 @@ import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
 
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -25,9 +31,9 @@ const SignUp: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
-
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      const errors = getValidationsErrors(err);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -38,7 +44,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
