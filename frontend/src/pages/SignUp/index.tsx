e9c56da27f62b5dbfe50ff9033e200e6a747e5e3
Fernando Frascino <fernandofrascino@yahoo.com.br>
2020-07-22 10:39:57 -0300

    add: Validate sign up form

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import * as Yup from 'yup';
 
 import { Form } from '@unform/web';
 
@@ -11,9 +12,24 @@ import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
 
-  function handleSubmit(data: object): void {
-    console.log('olá');
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 caracteres'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <Container>
