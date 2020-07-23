8037ee76f59416fe104a4a479f71df7c842b858f
Fernando Frascino <fernandofrascino@yahoo.com.br>
2020-07-23 17:24:20 -0300

    add: Login authentication with useContext and localStorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import SingIn from './pages/SingIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
-    {/* <SignUp /> */}
-    <SingIn />
+    <AuthProvider>
+      <SingIn />
+      {/* <SignUp /> */}
+    </AuthProvider>
     <GlobalStyle />
   </>
 )
