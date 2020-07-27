252563690796b386c0ef6cea12d91e49ae02a735
Fernando Frascino <fernandofrascino@yahoo.com.br>
2020-07-27 08:40:50 -0300

    remove: Unused logs

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -15,15 +15,11 @@ usersRouter.post('/', async (request, response) => {
 
   const creataUser = new CreateUserService();
 
-  console.log('Criou o Service')
-
   const user = await creataUser.execute({
     name,
     email,
     password,
   });
-
-  console.log('Criou o User');
 
   delete user.password;
 
