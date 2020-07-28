8d1bbf7991289690fce99fa7149585030456cc0e
Fernando Frascino <fernandofrascino@yahoo.com.br>
2020-07-28 20:08:33 -0300

    add: SignIn and SignUp pages on APP with authentication

--- app/src/pages/SignUp/index.tsx
+++ app/src/pages/SignUp/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
+
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -12,12 +25,61 @@ import {
   Container,
   Title,
   BackToSignIn,
-  BackToSignInText
+  BackToSignInText,
+  Form
 } from './styles';
+
+interface SignUpData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
 
+  const formRef = useRef<FormHandles>(null);
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
+
+
+  const handleSignUp = useCallback(async (data: SignUpData) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 caracteres'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await api.post('/users', data);
+
+      Alert.alert(
+        'Cadastro realizado com sucesso',
+        'Você já pode fazer login na aplicação',
+      );
+
+      navigation.navigate('SignIn');
+
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+        return;
+      };
+
+      Alert.alert(
+        'Erro no cadastro',
+        'Ocorreu um erro ao fazer cadastro, tente novamente',
+      );
+    }
+  }, [navigation]);
 
   return (
     <>
@@ -36,12 +98,47 @@ const SignUp: React.FC = () => {
             <View>
               <Title>Cria sua conta</Title>
             </View>
+            <Form
+              onSubmit={handleSignUp}
+              ref={formRef}
+            >
+              <Input
+                name='name'
+                icon='user'
+                placeholder='Nome'
+                autoCapitalize='words'
+                autoCorrect={false}
+                returnKeyType='next'
+                onSubmitEditing={() => emailInputRef.current?.focus()}
+              />
 
-            <Input name='name' icon='user' placeholder='Nome' />
-            <Input name='email' icon='mail' placeholder='E-mail' />
-            <Input name='password' icon='lock' placeholder='Senha' />
+              <Input
+                ref={emailInputRef}
+                name='email'
+                icon='mail'
+                placeholder='E-mail'
+                autoCapitalize='none'
+                autoCorrect={false}
+                keyboardType='email-address'
+                returnKeyType='next'
+                onSubmitEditing={() => passwordInputRef.current?.focus()}
+              />
 
-            <Button onPress={() => { console.log('') }}>Cadastrar</Button>
+              <Input
+                ref={passwordInputRef}
+                name='password'
+                icon='lock'
+                placeholder='Senha'
+                secureTextEntry
+                textContentType='newPassword'
+                returnKeyType='send'
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
+
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Cadastrar
+              </Button>
+            </Form>
 
           </Container>
         </ScrollView>
