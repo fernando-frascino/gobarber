74dddb2d9eb3de6b720d2fd46aa8ba20ce14b2f1
Fernando Frascino <fernandofrascino@yahoo.com.br>
2020-08-17 20:13:35 -0300

    refactoring: moving routes logic to controllers

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,35 +1,13 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import enrureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController;
 
 appointmentsRouter.use(enrureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-  //   const appointments = await appointmentsRepository.find();
+appointmentsRouter.post('/', appointmentsController.create);
 
-  //   return response.json(appointments);
-  // })
-
-  appointmentsRouter.post('/', async (request, response) => {
-  const appointmentsRepository = new AppointmentsRepository();
-  const createAppointment = new CreateAppointmentService(appointmentsRepository);
-
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date)
-
-  const appointment = await createAppointment.execute({
-    provider_id,
-    date: parsedDate
-  })
-
-  return response.json(appointment);
-});
-
-export default appointmentsRouter
+export default appointmentsRouter;
